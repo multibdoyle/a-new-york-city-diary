87081ab65d5bd3fd7e55f92b19b7c11ed808e38f
Brendan <bdoyle.core@gmail.com>
2021-07-07 15:17:32 -0400

    added posts

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,7 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
-import Logo from './pic.jpg'
+import Logo from '../../static/skeleton_praying_128_tn.jpg'
 
 const Sidebar = ({ siteMetadata }) => (
   <>
